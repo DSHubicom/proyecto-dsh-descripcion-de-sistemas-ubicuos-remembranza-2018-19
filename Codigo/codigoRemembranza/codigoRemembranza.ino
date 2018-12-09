74e5ca96013361ad58de9a06636314c69efd4582
vblank space <35319456+ManuJazz@users.noreply.github.com>
2018-12-09 22:18:19 +0100

    Add files via upload

---
+++ Codigo/codigoRemembranza/codigoRemembranza.ino
@@ -0,0 +1,622 @@
+/***************************************************
+  This is an example for our Adafruit FONA Cellular Module
+
+  Designed specifically to work with the Adafruit FONA
+  ----> http://www.adafruit.com/products/1946
+  ----> http://www.adafruit.com/products/1963
+  ----> http://www.adafruit.com/products/2468
+  ----> http://www.adafruit.com/products/2542
+
+  These cellular modules use TTL Serial to communicate, 2 pins are
+  required to interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+/*
+THIS CODE IS STILL IN PROGRESS!
+
+Open up the serial console on the Arduino at 115200 baud to interact with FONA
+
+Note that if you need to set a GPRS APN, username, and password scroll down to
+the commented section below at the end of the setup() function.
+*/
+#include "Adafruit_FONA.h"
+
+#define FONA_RX 9
+#define FONA_TX 8
+#define FONA_RST 4
+
+// this is a large buffer for replies
+char replybuffer[255];
+
+// We default to using software serial. If you want to use hardware serial
+// (because softserial isnt supported) comment out the following three lines 
+// and uncomment the HardwareSerial line
+#include <SoftwareSerial.h>
+#include <DFPlayer_Mini_Mp3.h>
+
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+SoftwareSerial *fonaSerial = &fonaSS;
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+// Hardware serial is also possible!
+//  HardwareSerial *fonaSerial = &Serial1;
+
+// Use this for FONA 800 and 808s
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+// Use this one for FONA 3G
+//Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST);
+
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
+
+uint8_t type;
+
+bool ejecucion = false;
+
+void setup() {
+  while (!Serial);
+
+  Serial.begin(115200);
+  mySerial.begin (9600);
+  mp3_set_serial (mySerial);
+  mp3_set_volume (30);
+  Serial.println(F("FONA basic test"));
+  Serial.println(F("Initializing....(May take 3 seconds)"));
+
+  fonaSerial->begin(4800);
+  if (! fona.begin(*fonaSerial)) {
+    Serial.println(F("Couldn't find FONA"));
+    while (1);
+  }
+  type = fona.type();
+  Serial.println(F("FONA is OK"));
+  Serial.print(F("Found "));
+  switch (type) {
+    case FONA800L:
+      Serial.println(F("FONA 800L")); break;
+    case FONA800H:
+      Serial.println(F("FONA 800H")); break;
+    case FONA808_V1:
+      Serial.println(F("FONA 808 (v1)")); break;
+    case FONA808_V2:
+      Serial.println(F("FONA 808 (v2)")); break;
+    case FONA3G_E:
+      Serial.println(F("FONA 3G (European)")); break;
+    default: 
+      Serial.println(F("???")); break;
+  }
+  
+  // Print module IMEI number.
+  char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
+  uint8_t imeiLen = fona.getIMEI(imei);
+  if (imeiLen > 0) {
+    Serial.print("Module IMEI: "); Serial.println(imei);
+  }
+
+  // Optionally configure a GPRS APN, username, and password.
+  // You might need to do this to access your network's GPRS/data
+  // network.  Contact your provider for the exact APN, username,
+  // and password values.  Username and password are optional and
+  // can be removed, but APN is required.
+  //fona.setGPRSNetworkSettings(F("orangeworld"), F("orange"), F("orange"));
+  fona.setGPRSNetworkSettings(F("movistar.es"), F("MOVISTAR"), F("MOVISTAR"));
+
+  // Optionally configure HTTP gets to follow redirects over SSL.
+  // Default is not to follow SSL redirects, however if you uncomment
+  // the following line then redirects over SSL will be followed.
+  //fona.setHTTPSRedirect(true);
+
+  printMenu();
+}
+
+void printMenu(void) {
+  Serial.println(F("-------------------------------------"));
+  Serial.println(F("[?] Print this menu"));
+  Serial.println(F("[U] Unlock SIM with PIN code"));
+
+  // SMS
+  Serial.println(F("[u] Send USSD"));
+
+  // GPRS
+  Serial.println(F("[G] Enable GPRS"));
+  Serial.println(F("[g] Disable GPRS"));
+  Serial.println(F("[l] Query GSMLOC (GPRS)"));
+  Serial.println(F("[w] Read webpage (GPRS)"));
+  Serial.println(F("[W] Post to website (GPRS)"));
+
+  // GPS
+  if ((type == FONA3G_A) || (type == FONA3G_E) || (type == FONA808_V1) || (type == FONA808_V2)) {
+    Serial.println(F("[O] Turn GPS on (FONA 808 & 3G)"));
+    Serial.println(F("[o] Turn GPS off (FONA 808 & 3G)"));
+    Serial.println(F("[L] Query GPS location (FONA 808 & 3G)"));
+    if (type == FONA808_V1) {
+      Serial.println(F("[x] GPS fix status (FONA808 v1 only)"));
+    }
+    Serial.println(F("[E] Raw NMEA out (FONA808)"));
+  }
+  Serial.println(F("-------------------------------------"));
+  Serial.println(F(""));
+
+}
+void loop() {
+
+ if(!ejecucion){
+
+    //Turn on GPS
+    switchCase('O');
+    delay(10000);
+
+    //Unlock SIM
+    switchCase('U');
+    delay(1000);
+
+    //Turn on GPRS
+    switchCase('G');
+    delay(3000);
+
+    //GET petition
+    switchCase('w');
+    delay(3000);
+
+    //GET GPS location
+    switchCase('D');
+    delay(3000);
+    
+    ejecucion = true;
+ }
+ 
+  }
+
+void Speech(String arg){
+     if(arg == "DN"){
+      mp3_play (1);
+      delay (5000);
+     }
+     if(arg == "SA"){
+      mp3_play (4);
+      delay (5000);
+      }
+     if(arg == "GD"){
+      mp3_play (2);
+      delay (5000);
+     }
+     if(arg == "GI"){
+      mp3_play (3);
+      delay (5000);
+     }
+  }
+
+void switchCase (char command){
+   switch (command) {
+    case '?': {
+        printMenu();
+        break;
+      }
+
+    case 'U': {
+        // Unlock the SIM with a PIN code
+        char PIN[5]="";
+        flushSerial();
+        Serial.println(F("Enter 4-digit PIN"));
+        //readline(PIN, 3);
+        Serial.println(PIN);
+        Serial.print(F("Unlocking SIM card: "));
+        if (! fona.unlockSIM(PIN)) {
+          Serial.println(F("Failed"));
+        } else{
+          Serial.println(F("OK!"));
+        }
+        break;
+      }
+      
+    case 's': {
+        // send an SMS!
+        char sendto[21], message[141];
+        flushSerial();
+        Serial.print(F("Send to #"));
+        readline(sendto, 20);
+        Serial.println(sendto);
+        Serial.print(F("Type out one-line message (140 char): "));
+        readline(message, 140);
+        Serial.println(message);
+        if (!fona.sendSMS(sendto, message)) {
+          Serial.println(F("Failed"));
+        } else {
+          Serial.println(F("Sent!"));
+        }
+
+        break;
+      }
+
+    /*********************************** GPS (SIM808 only) */
+
+    case 'o': {
+        // turn GPS off
+        if (!fona.enableGPS(false))
+          Serial.println(F("Failed to turn off"));
+        break;
+      }
+    case 'O': {
+        // turn GPS on
+        if (!fona.enableGPS(true))
+          Serial.println(F("Failed to turn on"));
+        break;
+      }
+    case 'x': {
+        int8_t stat;
+        // check GPS fix
+        stat = fona.GPSstatus();
+        if (stat < 0)
+          Serial.println(F("Failed to query"));
+        if (stat == 0) Serial.println(F("GPS off"));
+        if (stat == 1) Serial.println(F("No fix"));
+        if (stat == 2) Serial.println(F("2D fix"));
+        if (stat == 3) Serial.println(F("3D fix"));
+        break;
+      }
+
+    case 'L': {
+        // check for GPS location
+        char gpsdata[120];
+        fona.getGPS(0, gpsdata, 120);
+        if (type == FONA808_V1)
+          Serial.println(F("Reply in format: mode,longitude,latitude,altitude,utctime(yyyymmddHHMMSS),ttff,satellites,speed,course"));
+        else 
+          Serial.println(F("Reply in format: mode,fixstatus,utctime(yyyymmddHHMMSS),latitude,longitude,altitude,speed,course,fixmode,reserved1,HDOP,PDOP,VDOP,reserved2,view_satellites,used_satellites,reserved3,C/N0max,HPA,VPA"));
+        Serial.println(gpsdata);
+
+        int index = 0;
+        String x="";
+        String y="";
+        for(int i=0; i< sizeof(gpsdata); i++){
+            if(index == 3)
+              x= x + gpsdata[i];
+            else if (index == 4)
+              y= y + gpsdata[i];
+
+            if(gpsdata[i] == ','){
+              index++;
+            }
+          }
+
+          Serial.print("x: ");
+          Serial.println(x);
+          Serial.print("y: ");
+          Serial.println(y);
+
+        break;
+      }
+
+          case 'D': {
+        // check for GPS location
+        char gpsdata[120];
+        fona.getGPS(0, gpsdata, 120);
+        if (type == FONA808_V1)
+          Serial.println(F("Reply in format: mode,longitude,latitude,altitude,utctime(yyyymmddHHMMSS),ttff,satellites,speed,course"));
+        else 
+          Serial.println(F("Reply in format: mode,fixstatus,utctime(yyyymmddHHMMSS),latitude,longitude,altitude,speed,course,fixmode,reserved1,HDOP,PDOP,VDOP,reserved2,view_satellites,used_satellites,reserved3,C/N0max,HPA,VPA"));
+        Serial.println(gpsdata);
+
+        int index = 0;
+        String x="";
+        String y="";
+        for(int i=0; i< sizeof(gpsdata); i++){
+            if(index == 3)
+              x= x + gpsdata[i];
+            else if (index == 4)
+              y= y + gpsdata[i];
+
+            if(gpsdata[i] == ','){
+              index++;
+            }
+          }
+
+          Serial.print("x: ");
+          Serial.println(x);
+          Serial.print("y: ");
+          Serial.println(y);
+        
+
+        char sendto[21]= ""; 
+        String message ="Salida en long: " + x + " y lat: " + y;
+        char sms[141]; 
+        message.toCharArray(sms,141);
+        flushSerial();
+        Serial.print(F("Send to #"));
+        //readline(sendto, 20);
+        Serial.println(sendto);
+        Serial.print(F("Type out one-line message (140 char): "));
+        //readline(message, 140);
+        Serial.println(sms);
+        if (!fona.sendSMS(sendto, sms)) {
+          Serial.println(F("Failed"));
+        } else {
+          Serial.println(F("Sent!"));
+        }
+
+        break;
+      }
+
+    case 'E': {
+        flushSerial();
+        if (type == FONA808_V1) {
+          Serial.print(F("GPS NMEA output sentences (0 = off, 34 = RMC+GGA, 255 = all)"));
+        } else {
+          Serial.print(F("On (1) or Off (0)? "));
+        }
+        uint8_t nmeaout = readnumber();
+
+        // turn on NMEA output
+        fona.enableGPSNMEA(nmeaout);
+
+        break;
+      }
+
+    /*********************************** GPRS */
+
+    case 'g': {
+        // turn GPRS off
+        if (!fona.enableGPRS(false))
+          Serial.println(F("Failed to turn off"));
+        break;
+      }
+    case 'G': {
+        // turn GPRS on
+        if (!fona.enableGPRS(true))
+          Serial.println(F("Failed to turn on"));
+        break;
+      }
+    case 'l': {
+        // check for GSMLOC (requires GPRS)
+        uint16_t returncode;
+
+        if (!fona.getGSMLoc(&returncode, replybuffer, 250))
+          Serial.println(F("Failed!"));
+        if (returncode == 0) {
+          Serial.println(replybuffer);
+        } else {
+          Serial.print(F("Fail code #")); Serial.println(returncode);
+        }
+
+        break;
+      }
+    case 'w': {
+        // read website URL
+        uint16_t statuscode;
+        int16_t length;
+        String url = "destination-api-remembranza.herokuapp.com/J985/Service_01_Destination_API/1.0.0/destination/1";
+        //char url[sizeof(aux)]="https://destination-api-remembranza.herokuapp.com/J985/Service_01_Destination_API/1.0.0/destination";
+
+        flushSerial();
+        Serial.println(F("NOTE: in beta! Use small webpages to read!"));
+        Serial.println(F("URL to read (e.g. www.adafruit.com/testwifi/index.html):"));
+        Serial.print(F("http://")); 
+        Serial.println(url);
+        char aux[100];
+
+        url.toCharArray(aux,93);
+
+        Serial.println(aux);
+        Serial.println(F("****"));
+        if (!fona.HTTP_GET_start(aux, &statuscode, (uint16_t *)&length)) {
+          Serial.println("Failed!");
+          break;
+        }
+
+        char json [74];
+        int l=0;
+        int index=0;
+
+        while (length > 0) {
+          while (fona.available()) {
+            char c = fona.read();
+            Serial.write(c);
+            json[l]=c;
+            l++; 
+            length--;
+            if (! length) break;
+          }
+        }
+        Serial.println(json);
+        String dest="";
+        for(int i=0; i< (sizeof(json)-2); i++){
+            if(index == 9)
+              dest= dest + json[i];
+
+            if(json[i] == '\"'){
+              index++;
+            }
+          }
+
+        Serial.println(dest);
+        int beginIdx = 0;
+        //Get each instruction
+        int idx = dest.indexOf(",");
+        String arg="";
+        //Get each instruction
+        while (idx != -1){
+            delay(100);
+            arg = dest.substring(beginIdx, idx);
+            Serial.println(arg);
+            //Get position where instruction ends
+            int idx2= arg.indexOf("[");
+            String x_position = arg.substring(0, idx2);
+            String x_position = arg.substring(1, idx2);
+            //Get current user's position
+            char gpsdata[120];
+            fona.getGPS(0, gpsdata, 120);
+            int index = 0;
+            String x="";
+            String y="";
+            while(x!=x_position && y!=y_position){
+                Speech(arg);
+                for(int i=0; i< sizeof(gpsdata); i++){
+                  if(index == 3)
+                    x= x + gpsdata[i];
+                  else if (index == 4)
+                    y= y + gpsdata[i];
+      
+                  if(gpsdata[i] == ','){
+                    index++;
+                  }
+                }
+                //If position is out of permited range message is sent
+                if(x > 36.4864846 || y > -6.1184475){
+                    char sendto[21]= ""; 
+                    String message ="Salida en long: " + x + " y lat: " + y;
+                    char sms[141]; 
+                    message.toCharArray(sms,141);
+                    flushSerial();
+                    Serial.print(F("Send to #"));
+                    //readline(sendto, 20);
+                    Serial.println(sendto);
+                    Serial.print(F("Type out one-line message (140 char): "));
+                    //readline(message, 140);
+                    Serial.println(sms);
+                    if (!fona.sendSMS(sendto, sms)) {
+                      Serial.println(F("Failed"));
+                    } else {
+                      Serial.println(F("Sent!"));
+                    }
+                }
+                delay(30000);
+            }
+            beginIdx = idx + 1;
+            idx = dest.indexOf(",", beginIdx);
+        }
+        mp3_play (5);
+        delay (25000);
+        Serial.println(F("\n****"));
+        fona.HTTP_GET_end();
+        break;
+      }
+
+    case 'W': {
+        // Post data to website
+        uint16_t statuscode;
+        int16_t length;
+        char url[80];
+        char data[80];
+
+        flushSerial();
+        Serial.println(F("NOTE: in beta! Use simple websites to post!"));
+        Serial.println(F("URL to post (e.g. httpbin.org/post):"));
+        Serial.print(F("http://")); readline(url, 79);
+        Serial.println(url);
+        Serial.println(F("Data to post (e.g. \"foo\" or \"{\"simple\":\"json\"}\"):"));
+        readline(data, 79);
+        Serial.println(data);
+
+        Serial.println(F("****"));
+        if (!fona.HTTP_POST_start(url, F("text/plain"), (uint8_t *) data, strlen(data), &statuscode, (uint16_t *)&length)) {
+          Serial.println("Failed!");
+          break;
+        }
+        while (length > 0) {
+          while (fona.available()) {
+            char c = fona.read();
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+            loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
+            UDR0 = c;
+#else
+            Serial.write(c);
+#endif
+
+            length--;
+            if (! length) break;
+          }
+        }
+        Serial.println(F("\n****"));
+        fona.HTTP_POST_end();
+        break;
+      }
+    /*****************************************/
+
+    default: {
+        Serial.println(F("Unknown command"));
+        printMenu();
+        break;
+      }
+  } 
+
+
+
+  
+  // flush input
+  flushSerial();
+  while (fona.available()) {
+    Serial.write(fona.read());
+  }
+
+}
+
+void flushSerial() {
+  while (Serial.available())
+    Serial.read();
+}
+
+char readBlocking() {
+  while (!Serial.available());
+  return Serial.read();
+}
+uint16_t readnumber() {
+  uint16_t x = 0;
+  char c;
+  while (! isdigit(c = readBlocking())) {
+    //Serial.print(c);
+  }
+  Serial.print(c);
+  x = c - '0';
+  while (isdigit(c = readBlocking())) {
+    Serial.print(c);
+    x *= 10;
+    x += c - '0';
+  }
+  return x;
+}
+
+uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout) {
+  uint16_t buffidx = 0;
+  boolean timeoutvalid = true;
+  if (timeout == 0) timeoutvalid = false;
+
+  while (true) {
+    if (buffidx > maxbuff) {
+      //Serial.println(F("SPACE"));
+      break;
+    }
+
+    while (Serial.available()) {
+      char c =  Serial.read();
+
+      //Serial.print(c, HEX); Serial.print("#"); Serial.println(c);
+
+      if (c == '\r') continue;
+      if (c == 0xA) {
+        if (buffidx == 0)   // the first 0x0A is ignored
+          continue;
+
+        timeout = 0;         // the second 0x0A is the end of the line
+        timeoutvalid = true;
+        break;
+      }
+      buff[buffidx] = c;
+      buffidx++;
+    }
+
+    if (timeoutvalid && timeout == 0) {
+      //Serial.println(F("TIMEOUT"));
+      break;
+    }
+    delay(1);
+  }
+  buff[buffidx] = 0;  // null term
+  return buffidx;
+}
